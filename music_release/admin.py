c02a4f01eb3986bcc5a0d217e46d5a615ebcc6aa
shurushku <nastia.kuchina.741456963@gmail.com>
2022-11-10 12:07:09 +0200

    Create admin panel

--- music_release/admin.py
+++ music_release/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Genre, Song, Album, Artist
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Song)
+admin.site.register(Album)
+
+
+@admin.register(Artist)
+class DriverAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "pseudonym",
+                    )
+                },
+            ),
+        )
+    )
