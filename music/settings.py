bb1a26a13a452a695101c8a9d3cf6ba5504d848f
shurushku <nastia.kuchina.741456963@gmail.com>
2022-11-10 12:00:41 +0200

    Create models

--- music/settings.py
+++ music/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "music_release.Artist"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
--- music_release/models.py
+++ music_release/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from music.settings import AUTH_USER_MODEL
+
+
+class Genre(models.Model):
+    genre = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ["genre"]
+
+    def __str__(self):
+        return f"{self.genre}"
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    lyrics = models.TextField(blank=True)
+    length = models.IntegerField(validators=[MinValueValidator(0)])
+    genres = models.ManyToManyField(to=Genre)
+
+    def __str__(self):
+        return self.title
+
+
+class Artist(AbstractUser):
+    pseudonym = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["pseudonym"]
+
+    def __str__(self):
+        return f"{self.pseudonym} ({self.first_name} {self.last_name})"
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=255)
+    artists = models.ManyToManyField(to=AUTH_USER_MODEL)
+    length = models.IntegerField(validators=[MinValueValidator(0)])
+    release_date = models.DateField()
+    songs = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return f"{self.title} ({self.release_date})"
