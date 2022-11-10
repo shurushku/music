aa92fefe202d56cfb2e3095faacea10c8ad636bd
shurushku <nastia.kuchina.741456963@gmail.com>
2022-11-10 19:15:48 +0200

    Create registration page

--- music/urls.py
+++ music/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("music_release.urls", namespace="music_release")),
+    path("accounts/", include("django.contrib.auth.urls")),
 
     path('__debug__/', include('debug_toolbar.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
