e619775d30612c6ef0521dcf147b11680d8c7db4
shurushku <nastia.kuchina.741456963@gmail.com>
2022-11-12 17:05:47 +0200

    Change DEBUG un settings.py

--- music/settings.py
+++ music/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1", "https://music-service-2dkw.onrender.com/"]
 
