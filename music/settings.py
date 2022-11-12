c6c0342eb27ca8833aebe7772254c44305337c39
shurushku <nastia.kuchina.741456963@gmail.com>
2022-11-12 19:42:03 +0200

    Change TOOLBAR_CONFIG

--- music/settings.py
+++ music/settings.py
@@ -13,7 +13,6 @@ import os
 from pathlib import Path
 
 import dj_database_url
-from django.contrib import staticfiles
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,6 +80,15 @@ TEMPLATES = [
 WSGI_APPLICATION = "music.wsgi.application"
 
 
+def show_toolbar(request):
+    if DEBUG:
+        return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+}
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -136,7 +144,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = "staticfiles/"
+STATIC_ROOT = "/staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
