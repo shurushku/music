bd18966324eb29ac1c67f16d1aa429d45c291826
shurushku <nastia.kuchina.741456963@gmail.com>
2022-11-12 14:16:05 +0200

    Add tests

--- music_release/tests.py
+++ music_release/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+ARTIST_LIST_URL = reverse("music_release:artist-list")
+ALBUM_LIST_URL = reverse("music_release:album-list")
+GENRE_LIST_URL = reverse("music_release:genre-list")
+
+
+class PublicTests(TestCase):
+    def test_genre_login_required(self):
+        response = self.client.get(GENRE_LIST_URL)
+
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_album_list_login_required(self):
+        response = self.client.get(ALBUM_LIST_URL)
+
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_artist_login_required(self):
+        response = self.client.get(ARTIST_LIST_URL)
+
+        self.assertNotEqual(response.status_code, 200)
